d7309203df3f772962d586fc7bab1a0f6acbd5fb
cesar.carvalhal <cesar.carvalhal@netexlearning.com>
2023-04-05 15:17:10 +0200

    Implementing .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+// CONFIG
+import { config } from 'dotenv';
+config();
+
 // ROUTES
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,7 +15,7 @@ import { Activity, ActivitySchema } from './schemas/activities.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/db-test'),
+    MongooseModule.forRoot('mongodb://' + process.env.URL + process.env.DBNAME),
     MongooseModule.forFeature([{ name: Activity.name, schema: ActivitySchema }]),
   ],
   controllers: [AppController, ActivitiesController],
