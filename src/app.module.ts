04bcbe3bfc29aba544dd768ae8947ac32e3ee09b
Adrián Borrageiros Mourelos <adrian.borrageiros@netexlearning.com>
2023-04-05 15:07:42 +0200

    Mongo and env initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+// CONFIG
+import { config } from 'dotenv';
+config();
+
+// ROUTES
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://' + process.env.URL + process.env.DBNAME),
+  ],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
