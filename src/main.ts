015dd61e13a2d908a311d28b370de26b2d605126
cesar.carvalhal <cesar.carvalhal@netexlearning.com>
2023-05-12 09:54:38 +0200

    Fixed bug with cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+
 import { setupSwagger } from './swagger.builder';
 
+// import * as fs from 'fs';
+
 async function bootstrap() {
+  //const httpsOptions = {
+  //key: fs.readFileSync('/etc/letsencrypt/live/vm15.netexlearning.cloud-0001/privkey.pem'),
+
+  // cert: fs.readFileSync('/etc/letsencrypt/live/vm15.netexlearning.cloud-0001/fullchain.pem'),
+  //};
+
+  //const app = await NestFactory.create(AppModule, { httpsOptions });
+
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   setupSwagger(app);
-  await app.listen(3001);
+
+  await app.listen(443);
+
 }
+
 bootstrap();
