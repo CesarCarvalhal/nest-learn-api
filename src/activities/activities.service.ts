1560d58e5b7a2b9771d073313b46791596836913
Adrián Borrageiros Mourelos <adrian.borrageiros@netexlearning.com>
2023-05-16 09:26:33 +0200

    Check the answer with IA

--- src/activities/activities.service.ts
+++ src/activities/activities.service.ts
@@ -4,6 +4,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Activity, ActivityDocument } from './activities.schema';
 import { NotFoundException } from '@nestjs/common';
+import { Configuration, OpenAIApi } from "openai";
 
 @Injectable()
 export class ActivitiesService {
@@ -72,17 +73,58 @@ export class ActivitiesService {
   async checkAnswer(answer: string, activityData: Partial<Activity>): Promise<boolean> {
     var result = false;
     
-    if (activityData.type !== "Text"){
-      // True/False
-      if (activityData.type === "True/False"){
-        if (answer === activityData.isTrue.toString()){ result = true }
+    // Plain text
+    if (activityData.type === "Text"){
+      try {
+        result = await this.openaiCheck(answer, activityData.content)
+      } catch (error) {
+        result = false;
+      }
+    }
+
+    // True/False
+    if (activityData.type === "True/False"){
+      if (answer === activityData.isTrue.toString()){ result = true }
+    }
+
+    // Multiple options
+    if (activityData.type === "Multiple options" ){
+      const correctAnswer = activityData.options.find(option => option.correct === true);
+      if (answer === correctAnswer.text){ result = true }
+    }
+
+    return result;
+  }
+
+  async openaiCheck(answer: string, question: string): Promise<boolean> {
+    var result = false;
+
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Dada la siguiente pregunta: "${question}";
+                esta respuesta sería correcta?: "${answer}";
+                Responde true o false, en minusculas y sin ningun signo ortrográfico`,
+        temperature: 0.5,
+        max_tokens: 5,
+        // top_p: 1.0,
+        // frequency_penalty: 0.0,
+        // presence_penalty: 0.0,
+      });
+
+      if (response.data.choices[0].text.includes("false")) {
+        result = false;
+      } else if (response.data.choices[0].text.includes("true")) {
+        result = true;
       }
 
-      // Multiple options
-      if (activityData.type === "Multiple options" ){
-        const correctAnswer = activityData.options.find(option => option.correct === true);
-        if (answer === correctAnswer.text){ result = true }
-      }
+    } catch (error) {
+      console.log("Error: "+error.response.data);
     }
     return result;
   }
